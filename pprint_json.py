8661bf167b0bb88c49df701ef1fa5c37f8c979d7
Юлия Ларионова <yuliya_larionova@inbox.ru>
2016-10-27 09:50:25 +0300

    решение задачи

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
 import json
-
+import os
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open (filepath) as input_datas:
+        loaded_data=json.load(input_datas)
+        return loaded_data
 
-
+        
 def pretty_print_json(data):
-    pass
-
+    if data is not None:
+        formatted_data=json.dumps(data, ensure_ascii = False, sort_keys=True, indent=4)
+        print(formatted_data)
+    else:
+        print('Неправильно введен путь до файла или имя файла')
 
 if __name__ == '__main__':
-    pass
+    filepath=input('Введите путь до файла ')
+    pretty_print_json(load_data(filepath))     
+     
